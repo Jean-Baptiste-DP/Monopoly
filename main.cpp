a3a7515243d3decd102e9ed35b4f329c537e6166
Ilona-cormont <ilona.cormont@gmail.com>
2021-12-26 12:37:15 +0100

    Ajout tests

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include "Case.h"
+#include "Joueur.h"
 
 int main() {
+    Joueur joueur1("toto");
+    std::cout << "test1" << std::endl;
+    cout<<"Nom joueur1 : "<<joueur1.getNom()<<", Solde joueur 1 :"<<joueur1.getSolde()<<endl;
+    joueur1.setSolde(500);
+    cout<<"Nouveau solde "<<joueur1.getNom()<<" : "<<joueur1.getSolde()<<endl;
+    joueur1.crediter(500);
+    cout<<"Nouveau solde après crédit"<<joueur1.getNom()<<" : "<<joueur1.getSolde()<<endl;
+    joueur1.debiter(100);
+    cout<<"Nouveau solde après débit"<<joueur1.getNom()<<" : "<<joueur1.getSolde()<<endl;
+
+    std::cout << "test2" << std::endl;
     Case intermediaire;
     Case case_finale("rue de la paix",&intermediaire);
     Case avant("champ élysées",&case_finale);
