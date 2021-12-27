8255473b8e8ae6029687224785fed25639ada484
Ilona-cormont <ilona.cormont@gmail.com>
2021-12-27 10:02:29 +0100

    Modification Tests

--- main.cpp
+++ main.cpp
@@ -10,19 +10,25 @@ int main() {
     joueur1.setSolde(500);
     cout<<"Nouveau solde "<<joueur1.getNom()<<" : "<<joueur1.getSolde()<<endl;
     joueur1.crediter(500);
-    cout<<"Nouveau solde après crédit"<<joueur1.getNom()<<" : "<<joueur1.getSolde()<<endl;
+    cout<<"Nouveau solde "<<joueur1.getNom()<<" apres credit : "<<joueur1.getSolde()<<endl;
     joueur1.debiter(100);
-    cout<<"Nouveau solde après débit"<<joueur1.getNom()<<" : "<<joueur1.getSolde()<<endl;
+    cout<<"Nouveau solde "<<joueur1.getNom()<<" apres debit : "<<joueur1.getSolde()<<endl;
 
     std::cout << "\ntest2" << std::endl;
     Case intermediaire;
     Case case_finale("rue de la paix",&intermediaire);
     Case avant("champ élysées",&case_finale);
-    Case debut("debut",&avant);
+    Depart debut("debut",&avant);
     case_finale.setSuivante(&debut);
 
     cout<<"Debut : "<<debut.getNom()<<endl;
     cout<<"Avant : "<<(*debut.getSuivante()).getNom()<<endl;
     cout<<"Fin : "<<(*(*debut.getSuivante()).deplacer(4)).getNom()<<endl;
     cout<<"Re début : "<<(*(*(*debut.getSuivante()).getSuivante()).getSuivante()).getNom()<<endl;
+
+    std::cout << "\ntest3" << std::endl;
+    debut.arreterSur(&joueur1, 0);
+    cout << "Nouveau solde  " << joueur1.getNom() << " apres case depart : " << joueur1.getSolde() << endl;
+    std::cout << "test4" << std::endl;
+
 }
