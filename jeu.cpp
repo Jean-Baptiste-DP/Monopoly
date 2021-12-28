c748be0233a61f2e1a1c968825c5e0c8dcf794ee
Ilona-cormont <ilona.cormont@gmail.com>
2021-12-28 22:46:17 +0100

    Modification fichier jeu.cpp après erreur manipulation

--- jeu.cpp
+++ jeu.cpp
@@ -1,9 +1,27 @@
 //
 // Created by jbdur on 14/12/2021.
 //
-
 #include "jeu.h"
+#include <iostream>
+#include <random>
 
 jeu::jeu(){
-     paquet=&ListeCarte();
-};
+    ListeCarte vrai_paquet;
+    paquet=&vrai_paquet;
+}
+
+jeu::jeu(int de) {
+    de=0;
+}
+
+void jeu::lancer_des(Joueur *joueur){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(1, 6);
+    de=distrib(gen);
+    cout<<"Resultat de : "<<de<<endl;
+}
+
+int jeu::getDe() const{
+    return de;
+}
